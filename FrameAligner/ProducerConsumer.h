b71b753a36947a1e6b162b1ddd5bbef2093bea56
Sean Warren <s.warren@garvan.org.au>
2017-10-27 11:01:30 +1100

    Use multithreaded producers

--- FrameAligner/ProducerConsumer.h
+++ FrameAligner/ProducerConsumer.h
@@ -6,32 +6,40 @@
 #include <functional>
 
 template<typename T, typename FP, typename FC>
-void ProducerConsumer(FP producer, FC consumer, size_t n)
+void ProducerConsumer(size_t n_producer, FP producer, FC consumer, size_t n)
 {
-   std::queue<T> buffer;
+   std::map<int, T> buffer;
    std::mutex m;
    std::condition_variable cv;
+   std::atomic<int> pi = 0;
+   std::vector<std::future<void>> t_producer;
 
-   auto t_producer = std::async([&]() {
-      for (size_t i = 0; i < n; i++)
-      {
-         T obj = producer(i);
+   for(int p=0; p<n_producer; p++)
+      t_producer.push_back(std::async([&]() {
+         int i;
+         while((i = pi++) < n)
+         {
+            T obj = producer(i);
 
-         std::unique_lock<std::mutex> lk(m);
-         cv.wait(lk, [&] { return buffer.size() < 5; });
-         buffer.push(obj);
-         lk.unlock();
-         cv.notify_all();
-      }
-   });
+            std::unique_lock<std::mutex> lk(m);
+            buffer[i] = obj;
+            lk.unlock();
+            cv.notify_all();
+
+            // Keep the buffer from getting too large
+            //lk.lock();
+            //cv.wait(lk, [&] { return buffer.size() < 5; });
+            //lk.unlock();
+         }
+      }));
 
    auto t_consumer = std::async([&]() {
       for (size_t i = 0; i < n; i++)
       {
          std::unique_lock<std::mutex> lk(m);
-         cv.wait(lk, [&] { return !buffer.empty(); });
-         T obj = buffer.front();
-         buffer.pop(); 
+         cv.wait(lk, [&] { return buffer.count(i) == 1; });
+         T obj = buffer[i];
+         buffer.erase(i);
          lk.unlock();
          cv.notify_all();
 
@@ -39,6 +47,8 @@ void ProducerConsumer(FP producer, FC consumer, size_t n)
       }
    });
 
-   t_producer.get();
+   for(auto& t : t_producer)
+      t.get();
+
    t_consumer.get();
 }
